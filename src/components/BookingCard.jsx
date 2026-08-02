4dd902b0b082c3e9b8bc39cb525fd441af59d1de
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-08-02 14:31:53 +0600

    create a post method and fetch bookings routes data and data get from mongodb databasae collections

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -33,6 +33,16 @@ const BookingCard = ({ room }) => {
           departureDate: new Date (departureDate),
         }
         console.log(bookingData);
+
+        const res = await fetch("http://localhost:5000/bookings", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(bookingData),
+        });
+        const data = await res.json();
+        console.log(data);
     }
 
   
