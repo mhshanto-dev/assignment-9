259483b23aeba7f09d500ca2417f3fe7de37bbc6
Md Mehedi Hasan Shanto <ll4146394@gmail.com>
2026-08-02 21:07:22 +0600

    cnfig matcher has some prblems now it solves first login but all features 4O4 fonund now it works

--- src/proxy.js
+++ src/proxy.js
@@ -1,28 +1,24 @@
 import { NextResponse } from 'next/server'
 import { auth } from './lib/auth'
 import { headers } from 'next/headers'
- 
-// This function can be marked `async` if using `await` inside
+
 export async function proxy(request) {
+  const session = await auth.api.getSession({
+    headers: await headers()
+  })
 
-    const session = await auth.api.getSession({ headers: await headers()
+  if (!session) {
+    return NextResponse.redirect(new URL('/login', request.url))
+  }
 
-     })
-     if(!session) {
-        return NextResponse.redirect(new URL('/login', request.url))
-     }
-    
-
-  return NextResponse.redirect(new URL('/home', request.url))
+ 
+  return NextResponse.next()
 }
- 
-// Alternatively, you can use a default export:
-// export default function proxy(request) { ... }
- 
+
 export const config = {
   matcher: [
     '/my-bookings',
     '/add-room',
-    '/rooms/:path',
+    '/rooms/:path', 
   ]
 }
